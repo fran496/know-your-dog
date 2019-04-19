d1be15f37ab7b13960afbd96375dc4b409ea7ed1
fran496 <franciscorivas496@gmail.com>
2019-04-19 13:51:44 -0400

    Developed a function that makes the background image change when user clicks on one of the dogs that are within the navigation bar.
    Change some class names, add IDs to unique and important elements, swapped one class for an ID. This functionality was implemented because It gives the user a great experience in the web page, in my opinion.

--- scripts/main.js
+++ scripts/main.js
@@ -1,5 +1,36 @@
 // This function hides the introduction and tinted background from the web page when user clicks on it
-$(".introductory-text").click(() => {
-  $(".introductory-text").hide();
-  $("#variable-background").removeClass("tinted-background");
-});
+function hideIntroduction() {
+  $("#introductory-text").click(function() {
+    $(this).hide();
+    $("#variable-background").removeClass("tinted-background");
+  });
+}
+
+// This function recognizes the dog that the user clicked on and changes the background according to that dog
+function changeBackground() {
+  let background = $("#variable-background");
+  let backgroundClass = background.attr("class");
+  $("nav .dog-face").click(function() {
+    let dogId = $(this).attr("id");
+    addDogBackground(background, dogId);
+  });
+}
+
+function addDogBackground(background, dogId) {
+  if (dogId === "corgi-face") {
+    background.removeClass("samoyed shiba");
+    background.toggleClass("corgi");
+  }
+  else if (dogId === "samoyed-face") {
+    background.removeClass("shiba corgi");
+    background.toggleClass("samoyed");
+  }
+  else if (dogId === "shiba-face") {
+    background.removeClass("corgi samoyed");
+    background.toggleClass("shiba");
+  }
+}
+
+
+$(hideIntroduction);
+$(changeBackground);
